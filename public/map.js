fb9894a4c01c6b4009abf7db2570bb9482fed2ae
c0010257 <76168271+c0010257@users.noreply.github.com>
2024-02-19 19:12:51 +0000

    implement heatmaps

--- public/map.js
+++ public/map.js
@@ -84,23 +84,37 @@ export async function addProductsToMap() {
   }); 
   for (const [type, products] of Object.entries(productsByType)) {
     await addProductsTypeToMap(products, type);
+    // FRAMES
+    fillPolygon(`${type}-polygons`, type);
+    outlinePolygon(`${type}-polygons`, type);
+    // HEATMAP
+    addHeatmapLayer(`${type}-points`, type);
   }
 }
 
 async function addProductsTypeToMap(products, type) {
-  let featureCollection = {
+  let polygonFeatureCollection = {
     type: "FeatureCollection",
     features: products.map((product) => ({
       type: "Feature",
       geometry: product.footprint,
     }))
   };
-  addPolygon(type, featureCollection);
-  fillPolygon(type, type);
-  outlinePolygon(type, type);
+  let pointFeatureCollection = {
+    type: "FeatureCollection",
+    features: products.map((product) => ({
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: product.centre != null ? product.centre.split(",").reverse() : [],
+      },
+    }))
+  };
+  addSource(`${type}-polygons`, polygonFeatureCollection);
+  addSource(`${type}-points`, pointFeatureCollection);
 }
 
-function addPolygon(title, data) {
+function addSource(title, data) {
   map.addSource(title, {
     type: "geojson",
     data: data,
@@ -133,6 +147,81 @@ function outlinePolygon(title, productType) {
       "line-width": 1,
     },
   });
+}
+
+function addHeatmapLayer(title, productType) {
+  map.addLayer({
+    id: `${title}-heatmap`,
+    type: "heatmap",
+    source: title,
+    // maxzoom: 9,
+    // paint: {
+    //   // Increase the heatmap weight based on frequency and property magnitude
+    //   'heatmap-weight': [
+    //       'interpolate',
+    //       ['linear'],
+    //       ['get', 'mag'],
+    //       0,
+    //       0,
+    //       6,
+    //       1
+    //   ],
+    //   // Increase the heatmap color weight weight by zoom level
+    //   // heatmap-intensity is a multiplier on top of heatmap-weight
+    //   'heatmap-intensity': [
+    //       'interpolate',
+    //       ['linear'],
+    //       ['zoom'],
+    //       0,
+    //       1,
+    //       9,
+    //       3
+    //   ],
+    //   // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+    //   // Begin color ramp at 0-stop with a 0-transparancy color
+    //   // to create a blur-like effect.
+    //   'heatmap-color': [
+    //       'interpolate',
+    //       ['linear'],
+    //       ['heatmap-density'],
+    //       0,
+    //       'rgba(33,102,172,0)',
+    //       0.2,
+    //       'rgb(103,169,207)',
+    //       0.4,
+    //       'rgb(209,229,240)',
+    //       0.6,
+    //       'rgb(253,219,199)',
+    //       0.8,
+    //       'rgb(239,138,98)',
+    //       1,
+    //       'rgb(178,24,43)'
+    //   ],
+    //   // Adjust the heatmap radius by zoom level
+    //   'heatmap-radius': [
+    //       'interpolate',
+    //       ['linear'],
+    //       ['zoom'],
+    //       0,
+    //       2,
+    //       9,
+    //       20
+    //   ],
+    //   // // Transition from heatmap to circle layer by zoom level
+    //   // 'heatmap-opacity': [
+    //   //     'interpolate',
+    //   //     ['linear'],
+    //   //     ['zoom'],
+    //   //     7,
+    //   //     1,
+    //   //     9,
+    //   //     0
+    //   // ]
+    // }
+  })
+   
+
+
 }
 
 //-----------CUSTOM POLYGONS---------
