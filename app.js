90ecd6a3509e2a162185b4668e4802a26a049fd9
Joby Else <113983712+Jobster9@users.noreply.github.com>
2024-02-21 14:28:32 +0000

    resolved conflicts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -18,6 +19,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+var sess = {
+  secret: "keyboard cat",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
