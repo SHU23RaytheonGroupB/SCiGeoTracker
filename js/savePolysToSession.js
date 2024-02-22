822505fb8f27960024aeff6f0e5c652f1404cc82
c2077151 <c2077151@hallam.shu.ac.uk>
2024-02-22 12:38:17 +0000

    Display files in folder

--- js/savePolysToSession.js
+++ js/savePolysToSession.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
+var selectedItems = [];
 
 function saveTo() {
-  const dir = "./localPolygons";
-  var fileFind = []
-  fs.readdir(dir, (err, files) => {
-    fileFind = files;
+  const dir = "./js/localPolygons";
+  var fileFind = [];
+  fileFind = fs.readdirSync(dir, (err, files) => {
+    if (err) {
+      console.log(err);
+    }
   });
+  console.log("TEST");
+  console.log(fileFind);
   return fileFind;
 }
 
-module.exports = saveTo;
+function itemChange(value, item){
+  if(value == true){
+    selectedItems.push(item);
+    console.log("Item added");
+    console.log(selectedItems);
+  }
+  else{
+    if(selectedItems.includes(item)){
+      selectedItems.splice(selectedItems.indexOf(item), 1)
+    }
+    console.log("Item removed");
+    console.log(selectedItems);
+  }
+}
+
+module.exports = saveTo, itemChange;
