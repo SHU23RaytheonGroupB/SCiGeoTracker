0b06473cfee1cc30f5de6736b595a087c112e34a
c0010257 <76168271+c0010257@users.noreply.github.com>
2024-03-20 16:05:47 +0000

    fix dragging

--- public/javascripts/dragging-divs.js
+++ public/javascripts/dragging-divs.js
@@ -52,8 +52,8 @@ function dragElement(ele) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    ele.style.top = (ele.offsetTop - pos2) + "px";
+    ele.style.left = (ele.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
