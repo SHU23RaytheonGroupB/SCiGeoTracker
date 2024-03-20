75f9ccb7b838f6a47cdb14d20d0e43110e790fb9
c0010257 <76168271+c0010257@users.noreply.github.com>
2024-03-20 13:02:48 +0000

    additional filter menu elements, open close functionality

--- public/javascripts/filter-menu.js
+++ public/javascripts/filter-menu.js
@@ -1,10 +1,27 @@
 export function initialiseFilterMenu() {
     const filterButtonEle = document.querySelector("#filter-button");
     const filterMenuContainerEle = document.querySelector("#filter-menu-container");
+    let filterMenuOpen = false;
     filterButtonEle.onclick = () => {
+      if (!filterMenuOpen) {
         filterMenuContainerEle.style.display = null;
+      } else {
+        filterMenuContainerEle.style.display = "none";
+      }
+      filterMenuOpen = !filterMenuOpen;
     };
     document.querySelector("#filter-menu-close-button").onclick = () => {
         filterMenuContainerEle.style.display = "none";
     };
-}
+    const filterMissionListButtonEle = document.querySelector("#filter-mission-list-button");
+    const filterMissionListContainerEle = document.querySelector("#filter-mission-list-container");
+    let missionListOpen = false;
+    filterMissionListButtonEle.onclick = () => {
+      if (!missionListOpen) {
+        filterMissionListContainerEle.style.display = null;
+      } else {
+        filterMissionListContainerEle.style.display = "none";
+      }
+      missionListOpen = !missionListOpen;
+    };
+  }
